a260e98fc55a65556187c46c8b5c7b0d5f2eb56c
diGit-hub <gabrielsud.m.s@gmail.com>
2026-03-06 03:29:38 -0300

    Implement search form functionality and enhance UI components

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,57 @@
  * Este bloque de código contiene la funcionalidad principal
  * que define el comportamiento del programa.
  */
+
+import { togglePopup, showAll, showStaysNumber, showFiltered, cleanCards, adjustGuests } from "./utils";
+
+// Show every stay at website opening
+showAll();
+showStaysNumber();
+
+
+// Logic for opening and closing the form all the ways possible
+const closeButton = document.querySelector("#close");
+closeButton.addEventListener("click", togglePopup);
+
+const clickableBar = document.querySelectorAll(".clickable-bar");
+clickableBar.forEach(element => {
+    element.addEventListener("click", togglePopup);
+});
+
+const backdrop = document.querySelector("#backdrop");
+backdrop.addEventListener("click", togglePopup);
+
+
+// Logic for increasing and decreasing the number of children-adults in the form
+const adults = document.querySelector("#adults");
+const children = document.querySelector("#children");
+
+document.querySelector("#adults-increase").addEventListener("click", () => {
+    adults.value = Number(adults.value) + 1;
+    adjustGuests();
+})
+document.querySelector("#adults-decrease").addEventListener("click", () => {
+    if(Number(adults.value) > 0){
+        adults.value = Number(adults.value) - 1;
+        adjustGuests();
+    }
+})
+document.querySelector("#children-increase").addEventListener("click", () => {
+    children.value = Number(children.value) + 1;
+    adjustGuests();
+})
+document.querySelector("#children-decrease").addEventListener("click", () => {
+    if(Number(children.value) > 0){
+        children.value = Number(children.value) - 1;
+        adjustGuests();
+    }
+})
+
+// Logic for submitting the form and reloading cards
+document.querySelector('#popup-form').addEventListener('submit', (event) => {
+    event.preventDefault();
+    cleanCards();
+    showFiltered();
+    togglePopup();
+    showStaysNumber();
+});
--- scripts/utils.js
+++ scripts/utils.js
@@ -3,3 +3,78 @@
  * Este archivo contiene funciones auxiliares que serán utilizadas y llamadas
  * desde el archivo principal para realizar varias operaciones.
  */
+
+import { stays } from "./stays";
+
+function cleanCards(){
+    const cards = document.querySelector("#cards");
+    cards.innerHTML = ``;
+}
+
+function showAll() {
+    cleanCards();
+    const cards = document.querySelector("#cards");
+    for(let index = 0; index < stays.length; index++){
+        cards.innerHTML += `
+            <div class="flex flex-col w-full card">
+                <img alt="Stay photography in ${stays[index].city}, ${stays[index].country}"
+                src="${stays[index].photo}" class="rounded-2xl w-full max-w-[288px] h-47.5 object-cover" width="288px" height="190px">
+
+                <div class="flex flex-col p-2 gap-1">
+                    <div class="flex justify-between">
+                        <p class="text-type-and-beds-text text-xs">${stays[index].type}. ${stays[index].beds ? stays[index].beds : "No"} beds</p>
+                        <div class="flex">
+                            <img alt="star icon" height="12px" width="12px" src="../images/icons/star.svg">
+                            <p class="text-xs font-light">${stays[index].rating}</p>
+                        </div>
+                    </div>
+                    <p class="text-sm font-semibold">${stays[index].title}</p>
+                </div>
+            </div>
+        `;
+    }
+}
+
+function showStaysNumber(){
+    const stays = document.querySelectorAll(".card");
+    const staysNumber = document.querySelector("#stays-number");
+    staysNumber.textContent = `+${stays.length} stays`;
+}
+
+function togglePopup(){
+    document.querySelector("#popup").classList.toggle("hidden");
+}
+
+function showFiltered(){
+    const cards = document.querySelector("#cards");
+    const guests = document.querySelector("#guests-popup")
+    const filtered = stays.filter(stay => stay.maxGuests >= guests.value )
+    for (let index = 0; index < filtered.length; index++) {
+        cards.innerHTML += `
+            <div class="flex flex-col w-full card">
+                <img alt="Stay photography in ${filtered[index].city}, ${filtered[index].country}"
+                src="${filtered[index].photo}" class="rounded-2xl w-full max-w-[288px] h-47.5 object-cover" width="288px" height="190px">
+
+                <div class="flex flex-col p-2 gap-1">
+                    <div class="flex justify-between">
+                        <p class="text-type-and-beds-text text-xs">${filtered[index].type}. ${filtered[index].beds ? filtered[index].beds : "No"} beds</p>
+                        <div class="flex">
+                            <img alt="star icon" height="12px" width="12px" src="../images/icons/star.svg">
+                            <p class="text-xs font-light">${filtered[index].rating}</p>
+                        </div>
+                    </div>
+                    <p class="text-sm font-semibold">${filtered[index].title}</p>
+                </div>
+            </div>
+        `;
+    }
+}
+
+function adjustGuests(){
+    const adults = document.querySelector("#adults");
+    const children = document.querySelector("#children");
+    const guests = document.querySelector("#guests-popup");
+    guests.value = Number(adults.value) + Number(children.value);
+}
+
+export { showAll, togglePopup, showStaysNumber, showFiltered, cleanCards, adjustGuests };
